4507cf449e6bb626f0541ea2cba8239237958d01
Indra-wahyudi <203040126@mail.unpas.ac.id>
2022-10-25 11:12:11 +0700

    Menyelesaikan vid 'Database Seeder'

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -3,8 +3,8 @@
 @section('container')
   <article>
     <h2>{{ $post->tittle }}</h2>
-    <p>By. in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none"class="text-decoration-none">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
+    <a href="/blog">Back to Posts</a>
   </article>
-  <a href="/blog">Back to Posts</a>
 @endsection
